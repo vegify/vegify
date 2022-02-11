7925b01f2e3e7154ae9bf95d0a1c0a11811fc225
John Carmack <johncarmack@me.com>
2022-02-11 00:31:45 -0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'yellow': '#ffd400',
+      'yellow-orange': '#fbb040',
+      'green': '#65a73e',
+      'forest-green': '#328432',
+      'gray-dark': '#2b2b2b',
+      'gray': '#8a8b8a',
+      'gray-light': '#f1f1f1',
+    },
+    fontFamily: {    },
+    extend: {
+      spacing: {      },
+      borderRadius: {      }
+    },
+  },
+  plugins: [],
+}
